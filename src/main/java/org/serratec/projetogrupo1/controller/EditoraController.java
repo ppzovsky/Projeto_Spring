58093da6e5806f13fd38bad8240d97889838178b
Aosil803 <145231518+Aosil803@users.noreply.github.com>
2024-05-14 14:22:35 -0300

    classe editora

---
+++ src/main/java/org/serratec/projetogrupo1/controller/EditoraController.java
@@ -0,0 +1,10 @@
+package org.serratec.projetogrupo1.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("/editora")
+public class EditoraController {
+
+}
